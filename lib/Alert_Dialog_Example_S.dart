710857a729fd2645d21f44e7d449fe713e1f2109
SayedKhaledMarey <sayedmarey333@gmail.com>
2024-06-03 13:38:00 +0300

    finish 6

---
+++ lib/Alert_Dialog_Example_S.dart
@@ -0,0 +1,33 @@
+library package_example;
+import 'package:flutter/material.dart';
+
+class CustomAlertBoxS {
+  static Future showCustomAlertBox({
+    required BuildContext context,
+    required Widget willDisplayWidget,
+  }) {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(25)),
+            ),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                willDisplayWidget,
+                MaterialButton(
+                  color: Colors.red,
+                  child: const Text('OK', style: TextStyle(color: Colors.white)),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                )
+              ],
+            ),
+            elevation: 10,
+          );
+        });
+  }
+}
